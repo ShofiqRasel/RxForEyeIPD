53c6918d8c563fb45109b1a19fc011a8054fc085
Shofiqul Islam <shofiq.rasel@gmail.com>
2026-03-30 00:18:31 +0600

    ssl certificate

--- RxForEyeIPD/wwwroot/CustomJs/DeviceInfo.js
+++ RxForEyeIPD/wwwroot/CustomJs/DeviceInfo.js
@@ -1,6 +1,18 @@
-﻿window.getDeviceInfo = () => {
-    return "" + window.screen.width +
-           "" + window.screen.height +
-           "" + window.innerWidth +
-           "" + window.innerHeight;
+﻿//window.getDeviceInfo = () => {
+//    return "" + window.screen.width +
+//           "" + window.screen.height +
+//           "" + window.innerWidth +
+//           "" + window.innerHeight;
+//};
+
+
+window.getDeviceInfo = () => {
+    let deviceId = localStorage.getItem("deviceId");
+
+        if (!deviceId) {
+            deviceId = crypto.randomUUID(); // ✅ unique per browser/device
+            localStorage.setItem("deviceId", deviceId);
+        }
+
+        return deviceId;
 };
